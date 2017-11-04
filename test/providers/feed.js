9eac808a6d6f4bbd967cb260bdb323c98d536c99
Sam Warren <sam.warren@flashtalking.com>
2017-11-04 17:22:00 +0000

    #SW Added tests for the feed provider

--- test/providers/feed.js
+++ test/providers/feed.js
@@ -1,5 +1,71 @@
 import assert from 'assert';
+import proxyquire from 'proxyquire';
+
+let feed;
+let createConnectionStub = () => {};
+
+beforeEach(() => {
+  feed = proxyquire('../../app/providers/feed', {
+    net: {
+      createConnection: () => { // eslint-disable-line arrow-body-style
+        return createConnectionStub(...arguments);
+      },
+    },
+  });
+});
 
 describe('providers/feed', () => {
+  it('Returns the a net socket', async () => {
+    const mockSocket = { test: true, on: () => {} };
 
+    createConnectionStub = () => mockSocket;
+
+    const socket = await feed.default();
+
+    assert.deepEqual(mockSocket, socket);
+  });
+
+  it('Doesnt create multiple sockets if called multiple times', async () => {
+    const mockSocket = { on: () => {} };
+    let counter = 0;
+
+    createConnectionStub = () => {
+      counter += 1;
+
+      return mockSocket;
+    };
+
+    await feed.default();
+    await feed.default();
+    await feed.default();
+
+    assert.equal(counter, 1);
+  });
+
+  it('Removes the socket reference if the socket ends', async () => {
+    let counter = 0;
+    let removeSocketFn;
+
+    const mockSocket = {
+      on: (name, fn) => {
+        if (name === 'end') {
+          removeSocketFn = fn;
+        }
+      },
+    };
+
+    createConnectionStub = () => {
+      counter += 1;
+
+      return mockSocket;
+    };
+
+    await feed.default();
+
+    removeSocketFn();
+
+    await feed.default();
+
+    assert.equal(counter, 2);
+  });
 });
